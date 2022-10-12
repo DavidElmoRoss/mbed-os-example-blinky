f1c957d7109f24c430821f088ecf141fde0fea5a
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2022-10-12 17:03:15 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(PTB22);       // RED LED
-    DigitalOut led1(PTE24);      // GREEN LED
+    DigitalOut led1(PTE26);      // GREEN LED
     DigitalOut led2(PTB21);      // BLUE LED
 
     led=led1=led2=1;            // shut off all leds
